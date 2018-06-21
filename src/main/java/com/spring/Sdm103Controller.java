1c0647c841d48751d5bbfc5df8d4f5a936a2ddbc
Christian Dupuis <cd@atomist.com>
2018-06-22 01:09:10 +0200

    Update Sdm103Controller.java

--- src/main/java/com/spring/Sdm103Controller.java
+++ src/main/java/com/spring/Sdm103Controller.java
@@ -19,7 +19,7 @@ package com.spring;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
  
 @RestController
