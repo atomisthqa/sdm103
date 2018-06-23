29699e3cd89deee6d741414be77feb8500fc332d
Christian Dupuis <cd@atomist.com>
2018-06-23 13:11:20 +0200

    Update Sdm103Application.java

--- src/main/java/com/spring/Sdm103Application.java
+++ src/main/java/com/spring/Sdm103Application.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sdm103Application {
        
-	public static void main(String[] args) {
+	public static void main( String[] args) {
 		SpringApplication.run(Sdm103Application.class, args);
 	}
 }
